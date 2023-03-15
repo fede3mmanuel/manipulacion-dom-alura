c8c0ed943f6cbfeb5df227e820c3b34341f0dffc
Emmanuel Sanchez <fede@fede.website>
2023-03-15 16:11:16 -0600

    Concluir la tarea

--- script.js
+++ script.js
@@ -31,9 +31,14 @@ btn.addEventListener("click", createTask)
 
 const checkComplete = () => {
     const i = document.createElement("i");
-    i.classList.add("far");
-    i.classList.add("fa-check-square");
-    i.classList.add("icon");
-
+    i.classList.add("far", "fa-check-square", "icon");
+    i.addEventListener("click", completeTask);
     return i;
+}
+
+const completeTask = (event) => {
+    const element = event.target
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far")
 }
